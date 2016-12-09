7166d0b3d5907477189de7dc80fdf724a322bd29
Julia Proft <juliamarieproft@gmail.com>
2016-12-09 12:21:58 -0500

    Added service for calibrating the Spheros

--- sphero_ball_real/src/robot_connector.py
+++ sphero_ball_real/src/robot_connector.py
@@ -2,7 +2,8 @@
 
 import rospy
 import sys
-from sphero_ball_real.srv import Connector
+from sphero_ball_real.srv import Connector, Calibration
+from sphero_node.srv import SetIntSrv, SetFloatSrv, StabilizationSrv
 from std_msgs.msg import Int32, ColorRGBA
 from rosgraph_msgs.msg import Log
 from subprocess import Popen
@@ -20,10 +21,11 @@ class RobotConnector:
   def __init__(self):
     self.connected = 0
     self.processes = {}
-    rospy.init_node('robot_setup')
+    rospy.init_node('robot_connector')
     rospy.Subscriber('rosout', Log, self.set_connection_state)
     rospy.Service('connect_sphero', Connector, self.connect_sphero)
     rospy.Service('disconnect_sphero', Connector, self.disconnect_sphero)
+    rospy.Service('calibrate_spheros', Calibration, self.calibrate_spheros)
     rospy.spin()
 
   def connect_sphero(self, data):
@@ -65,9 +67,26 @@ class RobotConnector:
     except:
       return 0
 
+  def calibrate_spheros(self, data):
+    """ Allows us to calibrate the Spheros' headings. YAY! """
+    for i in self.processes:
+      name = 'sphero' + str(i)
+      if data.calibrate:
+        rospy.Publisher(name + '/set_color', ColorRGBA, queue_size=1).publish(ColorRGBA(0,0,0,0))
+        rospy.ServiceProxy(name + '/sphero/set_stabilization', StabilizationSrv).call(0)
+        rospy.ServiceProxy(name + '/sphero/set_back_led', SetIntSrv).call(255)
+      else:
+        color = colors[-1] if i == special else colors[(i-1)%2]
+        rospy.ServiceProxy(name + '/sphero/set_heading', SetFloatSrv).call(0)
+        rospy.ServiceProxy(name + '/sphero/set_back_led', SetIntSrv).call(0)
+        rospy.ServiceProxy(name + '/sphero/set_stabilization', StabilizationSrv).call(1)
+        rospy.Publisher(name + '/set_color', ColorRGBA, queue_size=1).publish(color)
+    return 1
+
   def set_connection_state(self, log):
     """ Sets a status variable based on the connection success. """
-    if "Connect to Sphero with address" in log.msg:
+    #if "Connect to Sphero with address" in log.msg:
+    if "Connected to" in log.msg:
       self.connected = 1
     elif "Failed to connect to Sphero." in log.msg:
       self.connected = 0
--- sphero_ball_real/src/robot_setup.py
+++ sphero_ball_real/src/robot_setup.py
@@ -1,56 +1,23 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String, ColorRGBA
-from rosgraph_msgs.msg import Log
-from subprocess import Popen
-from nav_msgs.msg import Odometry
+from std_msgs.msg import String
 
 class RobotSetup:
 
-	def __init__(self):
-		self.n = rospy.get_param('total_robot_n')
-		rospy.init_node('robot_setup')
-		pub = rospy.Publisher('music', String, queue_size=10)
-		#self.bt = 0
-		#rospy.Subscriber('rosout', Log, self.set_bt_state)
-		#self.connect_to_spheros()
-		rospy.set_param('setup_complete', True)
-		raw_input('Press enter to begin the dance.')
-		pub.publish(String('begin'))
-		raw_input('Press enter to end the dance.')
-		pub.publish(String('end'))
-		rospy.spin()
-
-	def connect_to_spheros(self):
-		""" Loops until a Sphero is connected. """
-		i = 0
-		while i < self.n:
-			self.bt = 0
-			# Give the Sphero a unique name.
-			name = 'sphero'+str(i+1)
-			# Start a new sphero.py process to begin connecting.
-			p = Popen(['rosrun', 'sphero_node', 'sphero.py', '__ns:=' + name])
-			# Spin our wheels while connecting.
-			while not self.bt:
-				if p.poll():
-					break
-			if not p.returncode:
-				if self.bt == 1:
-					i += 1
-				elif self.bt == -1:
-					continue
-			else:
-				break
-
-	def set_bt_state(self, log):
-		""" Sets our Bluetooth variable based on the connection success. """
-		if "Connect to Sphero with address" in log.msg:
-			self.bt =  1
-		elif "Failed to connect to Sphero." in log.msg:
-			self.bt = -1
+  def __init__(self):
+    self.n = rospy.get_param('total_robot_n')
+    rospy.init_node('robot_setup')
+    pub = rospy.Publisher('music', String, queue_size=10)
+    # <connect to Spheros>
+    rospy.set_param('setup_complete', True)
+    raw_input('Press enter to begin the dance.')
+    pub.publish(String('begin'))
+    raw_input('Press enter to end the dance.')
+    pub.publish(String('end'))
+    rospy.spin()
 
 if __name__ == '__main__':
-	
-	RobotSetup()
-	
+  RobotSetup()
+
+
